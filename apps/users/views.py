7b92418d24ba4b8157c7d7f23c40eab0ff9e4577
wangchenming <charblus7582@gmail.com>
2019-07-08 08:40:23 +0800

    add login auth play email

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
+
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 # Create your views here.
 # 当我们配置url被这个view处理时，自动传入request对象
+
+from .models import UserProfile
+
+# 自定义登录，可使用邮箱和账号
+
+
+class CustomBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # 不希望用户存在两个，get只能有一个。两个是get失败的一种原因 Q为使用并集查询
+            user = UserProfile.objects.get(
+                Q(username=username) | Q(email=username))
+
+            # django的后台中密码加密：所以不能password==password
+            # UserProfile继承的AbstractUser中有def check_password(self, raw_password)
+
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
 
 
 def user_login(request):
--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
  ls
 git add .
 git status
-git commit -m  "add login template and auth"
+git commit -m  "add login auth play email"
 git pull origin master
 git push origin master
